2a75d99c9ddbb9648afaa079f2adc018c125b662
eric orellana <eric.orellana92@gmail.com>
2015-10-29 19:07:47 +0000

    added comments

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,10 +1,12 @@
 class Place < ActiveRecord::Base
 	belongs_to :user
-	validates :name, :presence => true, :length => { :minimum => 3}
-	validates :address, :description, :presence => true
+	has_many :comments
 
 	geocoded_by :address
 	after_validation :geocode
 	
+	validates :name, :presence => true, :length => { :minimum => 3}
+	validates :address, :description, :presence => true
+
 	self.per_page = 3
 end
