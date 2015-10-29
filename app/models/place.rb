e5092e422be4c9d1d136485183933856e28d1d68
eric orellana <eric.orellana92@gmail.com>
2015-10-29 18:08:26 +0000

    added geocoder

--- app/models/place.rb
+++ app/models/place.rb
@@ -2,5 +2,9 @@ class Place < ActiveRecord::Base
 	belongs_to :user
 	validates :name, :presence => true, :length => { :minimum => 3}
 	validates :address, :description, :presence => true
+
+	geocoded_by :address
+	after_validation :geocode
+	
 	self.per_page = 3
 end
