6f836b13d1c7d8a3aab891090953941a83470e92
eric orellana <eric.orellana92@gmail.com>
2015-11-09 15:57:09 +0000

    added controller test for user dashboard

--- test/factories.rb
+++ test/factories.rb
@@ -1,5 +1,9 @@
-# FactoryGirl.define do 
-# 	factory :comment do 
-		
-# 	end
-# end
+FactoryGirl.define do 
+	factory :user do 
+		sequence :email do |i|
+			"nomster_user#{i}@gmail.com"
+		end
+		password "bdaycake11"
+		password_confirmation "bdaycake11"
+	end
+end
